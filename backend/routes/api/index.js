07fe5ed4e5e811c25969000ce93bd2f2bfffd427
Daniel Yoo <dacapo0509@gmail.com>
2022-08-13 09:06:47 -0700

    added json

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,4 +7,8 @@ router.use(restoreUser);
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
+router.post("/test", function (req, res) {
+	res.json({ requestBody: req.body });
+});
+
 module.exports = router;
