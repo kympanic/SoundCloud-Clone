893344afc6bf962ba37a892b0c00a8d09ebf6fd7
Daniel Yoo <dacapo0509@gmail.com>
2022-08-14 01:42:52 -0700

    added associations and validations for comments table

--- backend/db/migrations/20220813183705-create-comment.js
+++ backend/db/migrations/20220813183705-create-comment.js
@@ -1,33 +1,38 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Comments', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      songId: {
-        type: Sequelize.INTEGER
-      },
-      body: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Comments');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Comments", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			songId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			body: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Comments");
+	},
 };
--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -1,25 +1,40 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Comment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Comment.init({
-    userId: DataTypes.INTEGER,
-    songId: DataTypes.INTEGER,
-    body: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Comment',
-  });
-  return Comment;
+	class Comment extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			Comment.belongsTo(models.User, {
+				foreignKey: "userId",
+			});
+			Comment.belongsTo(models.Song, {
+				foreignKey: "songId",
+			});
+		}
+	}
+	Comment.init(
+		{
+			userId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			songId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			body: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Comment",
+		}
+	);
+	return Comment;
 };
