7be66297a390841bc5f9e3d4ff756b03e21ccab0
Daniel Yoo <dacapo0509@gmail.com>
2022-08-14 14:02:03 -0700

    added validation and association to playlist model and migration

--- backend/db/migrations/20220813183800-create-playlist.js
+++ backend/db/migrations/20220813183800-create-playlist.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Playlists', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      previewImage: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Playlists');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Playlists", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			previewImage: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Playlists");
+	},
 };
--- backend/db/models/playlist.js
+++ backend/db/models/playlist.js
@@ -1,25 +1,36 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Playlist extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Playlist.init({
-    userId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    previewImage: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Playlist',
-  });
-  return Playlist;
+	class Playlist extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			Playlist.belongsToMany(models.Song, {
+				through: models.PlaylistSong,
+			});
+			Playlist.belongsTo(models.User, {
+				foreignKey: "userId",
+			});
+		}
+	}
+	Playlist.init(
+		{
+			userId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING,
+			},
+			previewImage: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: "Playlist",
+		}
+	);
+	return Playlist;
 };
