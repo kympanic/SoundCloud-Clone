fd648dece4d472c3d8f41a1d097a3c60a22f0446
Daniel Yoo <dacapo0509@gmail.com>
2022-08-23 12:59:59 -0700

    uncommented out needed requirement in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("express-async-errors");
 const morgan = require("morgan");
 const cors = require("cors");
-// const csurf = require("csurf");
+const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const { environment } = require("./config");
