b8291ed31d9e0b5a50799f9cd3bc48f36529fd68
Daniel Yoo <dacapo0509@gmail.com>
2022-08-23 12:57:32 -0700

    reinstated csurf

--- backend/app.js
+++ backend/app.js
@@ -31,15 +31,15 @@ app.use(
 );
 
 // Set the _csrf token and create req.csrfToken method
-// app.use(
-// 	csurf({
-// 		cookie: {
-// 			secure: isProduction,
-// 			sameSite: isProduction && "Lax",
-// 			httpOnly: true,
-// 		},
-// 	})
-// );
+app.use(
+	csurf({
+		cookie: {
+			secure: isProduction,
+			sameSite: isProduction && "Lax",
+			httpOnly: true,
+		},
+	})
+);
 
 app.use(routes);
 
