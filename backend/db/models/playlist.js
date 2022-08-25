37211217c13a6f8105f1125facdb542185524636
Daniel Yoo <dacapo0509@gmail.com>
2022-08-24 20:11:24 -0700

    added hooks true to belongstomany relationships

--- backend/db/models/playlist.js
+++ backend/db/models/playlist.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 		static associate(models) {
 			Playlist.belongsToMany(models.Song, {
 				through: models.PlaylistSong,
+				hooks: true,
 			});
 			Playlist.belongsTo(models.User, {
 				foreignKey: "userId",
