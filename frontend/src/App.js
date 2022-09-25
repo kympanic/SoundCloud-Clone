eba1eeae3563b6e4009349b70b057132045deab4
Daniel Yoo <dacapo0509@gmail.com>
2022-09-25 14:35:21 -0700

    loginpageform complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
 	return (
-		<>
-			<h1>hello from app</h1>
-		</>
+		<Switch>
+			<Route path="/login">
+				<LoginFormPage />
+			</Route>
+		</Switch>
 	);
 }
 
