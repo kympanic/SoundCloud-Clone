d83a15b80a7f3af84100298aa83394f555c8bbf5
Daniel Yoo <dacapo0509@gmail.com>
2022-09-27 18:44:38 -0700

    finished fixing errors for getting state of user for profile button. Changed api route get /api/session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -15,7 +15,7 @@ router.get("/", restoreUser, (req, res) => {
 		return res.json({
 			user: user.toSafeObject(),
 		});
-	} else return res.json({});
+	} else return res.json(null);
 });
 
 // Log User In
