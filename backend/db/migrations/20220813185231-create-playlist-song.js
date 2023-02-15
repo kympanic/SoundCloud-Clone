04d0437db4bfcd8defe4b184aafa610f1b96ce63
Daniel Yoo <dacapo0509@gmail.com>
2023-02-15 11:17:08 -0800

    got rid of options

--- backend/db/migrations/20220813185231-create-playlist-song.js
+++ backend/db/migrations/20220813185231-create-playlist-song.js
@@ -2,46 +2,42 @@
 
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		return await queryInterface.createTable(
-			"PlaylistSongs",
-			{
-				id: {
-					allowNull: false,
-					autoIncrement: true,
-					primaryKey: true,
-					type: Sequelize.INTEGER,
-				},
-				songId: {
-					type: Sequelize.INTEGER,
-					allowNull: false,
-					references: {
-						model: "Songs",
-						key: "id",
-					},
-					onDelete: "cascade",
-				},
-				playlistId: {
-					type: Sequelize.INTEGER,
-					allowNull: false,
-					references: {
-						model: "Playlists",
-						key: "id",
-					},
-					onDelete: "cascade",
-				},
-				createdAt: {
-					allowNull: false,
-					type: Sequelize.DATE,
-					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-				},
-				updatedAt: {
-					allowNull: false,
-					type: Sequelize.DATE,
-					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-				},
+		return await queryInterface.createTable("PlaylistSongs", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
 			},
-			options
-		);
+			songId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "Songs",
+					key: "id",
+				},
+				onDelete: "cascade",
+			},
+			playlistId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "Playlists",
+					key: "id",
+				},
+				onDelete: "cascade",
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+		});
 	},
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable("PlaylistSongs");
